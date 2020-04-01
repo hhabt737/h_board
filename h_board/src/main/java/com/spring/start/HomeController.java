48edd1d85d15e3f6402777752622e93b673e19c9
HyunheeKim <hhabt737@naver.com>
2020-04-02 02:15:58 +0900

    Add VO, DAO function

--- h_board/src/main/java/com/spring/start/HomeController.java
+++ h_board/src/main/java/com/spring/start/HomeController.java
@@ -1,39 +1,45 @@
 package com.spring.start;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.List;
 import java.util.Locale;
-
+ 
+import javax.inject.Inject;
+ 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+ 
+import com.example.dto.MemberVO;
+import com.example.service.MemberService;
+ 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
+    
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    
+    @Inject
+    private MemberService service;
+    
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) throws Exception{
+ 
+        logger.info("home");
+        
+        List<MemberVO> memberList = service.selectMember();
+        
+        model.addAttribute("memberList", memberList);
+ 
+        return "home";
+    }
+    
 }
+
--- h_board/src/main/webapp/WEB-INF/views/home.jsp
+++ h_board/src/main/webapp/WEB-INF/views/home.jsp
@@ -3,13 +3,30 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+    <h1>Hello world!</h1>
+ 
+    <table>
+        <thead>
+            <tr>
+                <th>아이디</th>
+                <th>비밀번호</th>
+                <th>이름</th>
+            </tr>
+        </thead>
+        <tbody>
+            <c:forEach items="${memberList}" var="member">
+                <tr>
+                    <td>${member.id}</td>
+                    <td>${member.pw}</td>
+                    <td>${member.name}</td>
+                </tr>
+            </c:forEach>
+        </tbody>
+    </table>
+ 
+ 
 </body>
 </html>
